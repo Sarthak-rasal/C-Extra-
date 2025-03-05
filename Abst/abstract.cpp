f4f8e33bd36484df6db27e4331803539547f5f68
Sarthak Rasal <varshanr75@gmail.com>
2025-03-05 20:50:59 +0530

    fixes

--- Abst/abstract.cpp
+++ Abst/abstract.cpp
@@ -9,10 +9,57 @@ public:
     virtual void prepareIngredients() = 0; // pure virtual fun
     virtual void brew() = 0;
     virtual void serve() = 0;
+
+    void makeTea()
+    {
+        prepareIngredients();
+        brew();
+        serve();
+    }
+};
+
+// derived
+
+class GreenTea : public Tea
+{
+public:
+    void prepareIngredients() override
+    {
+        cout << "Green leaves and water is ready" << endl;
+    }
+    void brew() override
+    {
+        cout << "Green Tea brewed" << endl;
+    }
+    void serve() override
+    {
+        cout << "Green Tea served" << endl;
+    }
+};
+class MasalaTea : public Tea
+{
+public:
+    void prepareIngredients() override
+    {
+        cout << "Spices,Tea leaves and water is ready" << endl;
+    }
+    void brew() override
+    {
+        cout << "Masala Tea brewed" << endl;
+    }
+    void serve() override
+    {
+        cout << "Masala Tea served" << endl;
+    }
 };
 
 int main()
 {
+    GreenTea greenTea;
+    MasalaTea masalaTea;
+
+    greenTea.makeTea();
+    masalaTea.makeTea();
 
     return 0;
 }
